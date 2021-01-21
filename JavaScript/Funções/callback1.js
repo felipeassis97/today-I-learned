b601bce5124af5acdef9b98085215ce3ccaef19f
felipeassis97 <44071207+felipeassis97@users.noreply.github.com>
2021-01-21 19:38:33 -0300

    Funções JS

---
+++ JavaScript/Funções/callback1.js
@@ -0,0 +1,5 @@
+const fabricantes = ["Audi", "Mercedes", "BMW"]
+
+function imprimir(nome, indice){
+    console.log(`${indice + 1}. ${nome}`)
+}
---
+++ JavaScript/Funções/factory.js
@@ -0,0 +1,7 @@
+function criarProduto(nome, preco){
+    return {
+        nome,
+        preco
+    }
+}
+console.log(criarProduto('Teste', 55.90))
