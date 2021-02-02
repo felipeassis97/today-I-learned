3a21ccf9cc48cb305d96b45bae1a5c3c510619be
felipeassis97 <44071207+felipeassis97@users.noreply.github.com>
2021-02-02 16:03:38 -0300

    Exercicios JS

--- JavaScript/Array/reduce.js
+++ JavaScript/Array/reduce.js
@@ -1,12 +1,22 @@
 const alunos = [
     { nome:'João', nota:7.3, bolsista:false },
-    { nome:'Maria', nota:9.2, bolsista:true },
+    { nome:'Maria', nota:9.2, bolsista:false },
     { nome:'Pedro', nota:9.8, bolsista:false },
-    { nome:'Ana', nota:8.7, bolsista:true }
+    { nome:'Ana', nota:8.7, bolsista:false }
 
 ]
 
+/*const todosBolsistas = function(resultado,bolsista){
+    return resultado && bolsista
+}*/
+/*const listaBolsista = function(lista){
+    return lista.bolsista
+}*/
+
 const todosBolsistas = (resultado,bolsista) =>  resultado && bolsista
+const algumBolsista = (resultado, bolsista) => resultado || bolsista
+const listaBolsista = (lista) => lista.bolsista
 
-console.log(alunos.map(a => a.bolsista)).reduce(todosBolsistas)
+const resultado = alunos.map(listaBolsista).reduce(algumBolsista)
 
+console.log(resultado)
