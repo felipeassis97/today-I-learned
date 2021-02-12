28e88601e6c972469a74412f11237078d89b765e
felipeassis97 <44071207+felipeassis97@users.noreply.github.com>
2021-02-12 17:32:20 -0300

    Exercicios JS

---
+++ JavaScript/Array/Exercicios/adicionaMais.js
@@ -0,0 +1,9 @@
+const adicionar = function(quantidade){
+    let resultado = ""
+    for (let i=0; i<quantidade; i++){
+        resultado += "+"
+    }
+    return console.log(resultado)
+}
+
+adicionar(8)
