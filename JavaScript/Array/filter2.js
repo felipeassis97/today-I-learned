b55132af3a54e2af1d654b97484619cfd0d63dd0
felipeassis97 <44071207+felipeassis97@users.noreply.github.com>
2021-02-15 17:40:27 -0300

    Exercicios JS

--- JavaScript/Array/filter2.js
+++ JavaScript/Array/filter2.js
@@ -9,7 +9,7 @@ Array.prototype.filter2 = function(callback){
 }
 
 const produtos = [
-    {nome: 'Notebook', preco: 2499, fragil: true},
+    {nome: 'MacBook', preco: 2499, fragil: true},
     {nome: 'Ipad pró', preco: 4199, fragil: false},
     {nome: 'Copo de vidro', preco: 12.99, fragil: true},
     {nome: 'Copo de plástico', preco: 18.99, fragil: false}
